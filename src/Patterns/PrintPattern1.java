9a6ab5137526d1f7ca782c1d9299041f7d9141e5
Jenelste-SJ <jenelste@gmail.com>
2025-12-22 16:36:26 +0530

    commit

--- src/Patterns/PrintPattern1.java
+++ src/Patterns/PrintPattern1.java
@@ -9,8 +9,6 @@ OUTPUT
 public class PrintPattern1 {
     public static void main(String[] args) {
 
-
-
         for (int i = 1; i <= 5; i++) {
             System.out.println("*");
         }
---
+++ src/Patterns/PrintPattern8.java
@@ -0,0 +1,7 @@
+package Patterns;
+
+public class PrintPattern8 {
+    public static void main(String[] args) {
+
+    }
+}
