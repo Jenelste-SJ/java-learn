6365fbe25723c37e68bf3d89a9c4624a61628c13
Jenelste-SJ <jenelste@gmail.com>
2026-01-02 17:01:35 +0530

    commit

--- src/OOPS/Static.java
+++ src/OOPS/Static.java
@@ -5,8 +5,14 @@ class Employee{
         id =20;
         System.out.println("I am working");
     }
+    static {
+        System.out.println("I am static");
+    }
+
+
     String name;
     public void attendMeeting(String name){
+
         System.out.println(name + "  is attending meeting");
     }
 }
@@ -15,6 +21,7 @@ public class Static {
     public static void main(String[] args) {
         Employee.work();
         System.out.println(Employee.id);
+
         Employee emp = new Employee();
         emp.name = "John";
         emp.attendMeeting(emp.name);
