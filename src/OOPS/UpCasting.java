398cb82ae2ebbd92c917b48930b47993a26853d1
Jenelste-SJ <jenelste@gmail.com>
2026-01-21 15:53:54 +0530

    commit

---
+++ src/OOPS/UpCasting.java
@@ -0,0 +1,23 @@
+package OOPS;
+
+
+class Device {
+    public void powerOn() {
+        System.out.println("Device is powered on.");
+    }
+}
+
+class Smartphone extends Device {
+    @Override
+    public void powerOn() {
+        System.out.println("Smartphone is powered on with a touch screen interface.");
+    }
+}
+
+public class UpCasting {
+    public static void main(String[] args) {
+        Device myDevice = new Smartphone(); // Upcasting
+        myDevice.powerOn();
+    }
+}
+
