dd8cb0c2a882ca4f8f4bed606df708457d19f496
Jenelste-SJ <jenelste@gmail.com>
2026-03-04 22:15:21 +0530

    commit

---
+++ src/com/learn/DSA/Collections/ArrayList1.java
@@ -0,0 +1,42 @@
+package com.learn.DSA.Collections;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList();
+        a.add(70);
+        a.add(90);
+        a.add(80);
+        a.add(70);
+        a.add(90);
+        // add with index
+        a.add(3,80);
+        //update the value
+        a.set(1,"SJ");
+        //get the value
+        System.out.println(a.get(0));
+        //find the size
+        System.out.println(a.size());
+        //remove at an index
+        a.remove(2);
+
+        //every value is an object
+        for(Object o : a){
+            System.out.println(o);
+        }
+
+        //new arrayList
+        ArrayList a1 = new ArrayList();
+        a1.add("hello");
+        a1.add("me");
+
+        //add all elements of 2nd arraylist to 1st arrayList
+        a1.addAll(a);
+        //print
+        System.out.println(a);
+        System.out.println(a1);
+        //check if the value contains
+        System.out.println(a1.contains("hello"));
+    }
+}
